186e5b71a524e9dc1e52cd8a6be53db888e5e8c0
Tenable.cs <admin@accurics.com>
2023-06-28 19:39:11 -0400

    Auto Remediation by Tenable.cs, file: aws/s3_bucket.tf

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -2,4 +2,30 @@ resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   bucket = "tenablecsdemos3bucket"
   acl    = "private"
   tags   = var.default_tags
+
+  versioning {
+    enabled = true
+  }
+
+  versioning {
+    mfa_delete = true
+    enabled    = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "<master_kms_key_id>"
+      }
+    }
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
